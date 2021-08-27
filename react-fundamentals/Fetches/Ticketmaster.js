a23346bed131598b004f87d20de526297f5c56af
Krystle Marshall <krystlemarshall@Krystles-Air.hsd1.in.comcast.net>
2021-08-26 21:19:37 -0400

    test2

--- react-fundamentals/Fetches/Ticketmaster.js
+++ react-fundamentals/Fetches/Ticketmaster.js
@@ -4,5 +4,5 @@ import React from 'react';
 const Ticketmaster = (props) => {
     const [data, setData] = useState("");
     const apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?geoPoint=39.89,-86.22&apikey=bi2M5AwJqAik73OnkxurIv7AjlHhlAdU"
-
+}
 export default Ticketmaster;
